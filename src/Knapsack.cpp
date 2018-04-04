482b860e7b3a7b86293a49b1072b1b1f9eb17be7
Nathan VandenHoek <vandenng@eos17.cis.gvsu.edu>
2018-04-04 17:09:26 -0400

    Added knapsackAlgorithm

--- src/Knapsack.cpp
+++ src/Knapsack.cpp
@@ -2,12 +2,28 @@
 #include <vector>
 
 
-std::vecotor<int> knapsack(std::vector<int> weight, std::vector<int> value, int maxWeight){
-	std::vector<int> finalValueList;
-	
-	for(int i = 0; i < weight.size(); ++i){
-		for(int j = 0; j < weight.size(); ++j){
-			
+int knapsack(std::vector<int> weight, std::vector<int> value, int maxWeight){
+	int finalValues[weight.size() + 1][maxWeight + 1];
+
+	for(unsigned int i = 0; i <= weight.size(); ++i){
+		for(int w = 0; w <= maxWeight; ++w){
+			if(i == 0 || w == 0)
+				finalValues[i][w] = 0;
+			else if (weight.at(i - 1) <= w){
+				finalValues[i][w] = std::max(value.at(i-1) + finalValues[i-1][w-weight.at(i-1)], finalValues[i-1][w]);
+				
+			}
+			else
+				finalValues[i][w] = finalValues[i-1][w];
 		}
-	}	
+	}
+	return finalValues[weight.size()][maxWeight];	
+}
+
+int main(int argc, char** argv){
+
+	std::vector<int> weight = {15, 25, 45, 30, 23, 32};
+	std::vector<int> value = {100, 350, 225, 67, 275, 168};
+	std::cout << knapsack(weight, value, 100) << std::endl;
+	return EXIT_SUCCESS;
 }
